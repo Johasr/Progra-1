0a9d5aa0acffb34f58a50ffe44c6bb1486ba6235
Johasr <Johanny-94-@hotmail.com>
2016-06-25 20:02:46 -0600

    Ahorcado

---
+++ Ahorcado/src/ahorcado/Ahorcado.java
@@ -0,0 +1,18 @@
+
+package ahorcado;
+import java.io.*;
+import java.util.Scanner;
+/**
+ *
+ * @author joha
+ */
+public class Ahorcado {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
